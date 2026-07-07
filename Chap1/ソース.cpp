33dc611d2653d418f3a73575faeb12a996ab6f1e
hmj2622016Noichi <hmj2622016@stu.o-hara.ac.jp>
2026-07-07 14:38:08 +0900

    dekita

---
+++ Chap1/ソース.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	//ƒRƒ“ƒ\[ƒ‹‚ÉHALLOW WORLD
+	std::cout << "Hello, world" << std::endl;
+
+	int value1(150);
+	int value2 = 150;
+	int value3{ 150 };
+	int value4 = { 159 };
+}
